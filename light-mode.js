54ed9f46af6847055a49890e9a40f80fbce922af
izerque <isaacmio118@gmail.com>
2020-06-24 00:39:41 +0300

    hard parts

--- light-mode.js
+++ light-mode.js
@@ -1,9 +1,30 @@
 let toggle= document.querySelector('#toggle_switch');
-let body=document.getElementsByTagName('body');
-let switcher=document.getElementsByClassName('.switcher');
+let body=document.getElementsByTagName('body')[0];
+let switcher=document.getElementsByClassName('switcher')[0];
+let headerbox=document.querySelector('#header_box');
+let headTitle=document.querySelector('#head-title');
+let card= document.getElementsByClassName('card');
+
+
+console.log(card);
+
+function carFunc(){
+    card.forEach(card.classList.toggle('card_light'));
+    return 0;
+
+}
+
+
+
 
 toggle.addEventListener( 'click' , function(event){
+    console.log(body)
     toggle.classList.toggle('light_mode');
     body.classList.toggle('lighter');
     switcher.classList.toggle('day');
+    headerbox.classList.toggle('dayHead');
+    headTitle.classList.toggle('heading_light');
+    card.classList.toggle('card_light')
+    
+
 });
